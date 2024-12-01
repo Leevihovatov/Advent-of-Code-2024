f5f9f9a05bfad2ebcae334a3eafeb6b8c2f364a7
Leevihovatov <leevi.hovatov@gmail.com>
2024-12-01 10:41:32 +0200

    day 1

---
+++ template.py
@@ -0,0 +1,14 @@
+def main():
+    with open("test.txt", "r") as f:
+        data = f.read().splitlines()
+    
+    silver = 0
+    gold = 0
+
+
+
+    print(f'Silver: {silver}')
+    print(f'Gold: {gold}')
+
+if __name__ == "__main__":
+    main()
